da9dbdac40b78bdb02a4adece981b4fba79c8650
Lebedenko Tatiana <lebedenko_tatiana@mail.ru>
2016-08-29 16:15:19 +0300

    first commit

---
+++ js/sqript.js
@@ -0,0 +1,23 @@
+﻿
+
+$(function() {
+  $('select').styler();
+});
+
+
+
+
+
+
+$(function() {
+
+  $('.menu li').hover(function() {
+ 
+			$(this).find('.subMenu:first').css({visibility: "visible",display: "none"}).show(600);
+		      }, 
+		      function() {
+			$(this).find('.subMenu:first').css({visibility: "hidden"});
+		      }
+  )
+
+});
